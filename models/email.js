6d4b1acb3a81c30d6b784908d7e9eadbacdd5c39
katcarr <kathrynelizabethcarr@gmail.com>
2015-03-05 21:34:45 -0800

    Add nem-mail template with non-functioning form

--- models/email.js
+++ models/email.js
@@ -1,5 +1,6 @@
 MeMail.Email = DS.Model.extend({
   author: DS.attr(),
   receiver: DS.attr(),
+  title: DS.attr(),
   folder : DS.belongsTo('email', {async: true})
 });
--- router.js
+++ router.js
@@ -1,3 +1,5 @@
 MeMail.Router.map(function(){
   this.resource('folders', {path: '/'});
+  this.resource('folder', {path: ':folder_id'});
+  this.resource('new-email');
 });
